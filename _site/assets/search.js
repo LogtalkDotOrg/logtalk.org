ed80d427ef7292177564cbaa00f1cf7fbb3c11f0
Paulo Moura <pmoura@logtalk.org>
2018-11-12 01:25:01 +0000

    Upstream changes to generated pages

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':'Post+with+Header+Image','url':'%2F2018%2F06%2F01%2Fheader-image.html'},{'title':'Welcome','url':'%2F2018%2F07%2F01%2Fwelcome.html'}]};
